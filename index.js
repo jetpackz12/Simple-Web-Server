7d776d9dd30c2db18882c7fe4130e3cf3edf12be
Joseph Jett Abela <jetpackzabela12@gmail.com>
2024-02-02 17:21:12 +0800

    Using-Express

--- index.js
+++ index.js
@@ -1,62 +1,26 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const path = require('path');
-const fs = require('fs');
 const { log } = require('console');
 
-const myServer = http.createServer((req, res) => {
-    
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+app.use(express.static('./public'));
 
-    let extName = path.extname(filePath);
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './pages/index.html'));
+});
 
-    let contentType = 'text/html';
-    switch (extName) {
-        case '.js':
-                contentType = 'text/javascript';
-            break;
-        case '.css':
-                contentType = 'text/css';
-            break;
-        case '.json':
-                contentType = 'application/json';
-            break;
-        case '.png':
-                contentType = 'image/png';
-            break;
-        case '.jpg':
-                contentType = 'image/jpg';
-            break;
-    }
+app.get('/about', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './pages/about.html'));
+});
 
-    fs.readFile(filePath, (err, content) => {
-        if (err) 
-        {
-            switch (err.code) {
-                case 'ENOENT':
-                        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-                            if(err) throw err;
+app.get('/contact', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './pages/contact.html'));
+});
 
-                            res.writeHead(200, { 'Content-Type' : 'text/html' });
-                            res.end(content, 'utf8');
-                        });
-                    break;
-            
-                default:
-                        res.writeHead(500);
-                        res.end(`Server error: ${err.code}`);
-                    break;
-            }
-            
-        } 
-        else 
-        {
-            res.writeHead(200, { 'Content-Type' : contentType });
-            res.end(content, 'utf8');
-        }
-    });
-
+app.get('*', (req, res) => {
+    res.status(404).sendFile(path.resolve(__dirname, './pages/404.html'));
 });
 
 const PORT = process.env.PORT || 5000;
 
-myServer.listen(PORT, () => log(`Server is running on ${PORT}`));
+app.listen(PORT, () => log(`Server startig on port: ${PORT}`));
